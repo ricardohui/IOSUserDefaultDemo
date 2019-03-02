9791d671b45fa60454c2a269210d7477f331b3d3
Ricardo <eehaha@gmail.com>
2019-03-02 14:53:44 +0800

    Persist String and Array using UserDefaults

--- UserDefaultDemo/ViewController.swift
+++ UserDefaultDemo/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    UserDefaults.standard.set("Rob", forKey: "name")
+        
+        
+        let nameAny = UserDefaults.standard.object(forKey:"name")
+        if let nameString = nameAny as? String{
+                 print(nameString)
+        }
+        let arr = [1,2,3,4]
+        UserDefaults.standard.set(arr, forKey:"array")
+        let arrayObject = UserDefaults.standard.object(forKey: "array")
+        if let array = arrayObject as? NSArray{
+            print(array)
+        }
     }
+    
+    
 
 
 }
